14553e6abf323644923760d06f62d1bdfb9becf1
Pooh3Mobi <pooh3.mobi@gmail.com>
2018-07-04 01:14:43 +0900

    clean

--- resultkt/src/main/java/mobi/pooh3/sample/Sample.kt
+++ resultkt/src/main/java/mobi/pooh3/sample/Sample.kt
@@ -1,12 +1,13 @@
 package mobi.pooh3.sample
 
+import android.support.annotation.IntRange
 import mobi.pooh3.resultkt.Result
 
 fun main(args: Array<String>) {
     val rep = Repository()
     val userId = UserId("test")
-    times(1) { rep.findCachedUserData(userId) }
-            .recover { times(3){ rep.findUserData(userId) } }
+    tryTimes(1) { rep.findCachedUserData(userId) }
+            .recover { tryTimes(3){ rep.findUserData(userId) } }
             .onSuccess{ handleSuccess() }
             .onFailure{ handleFailure() }
 
@@ -24,8 +25,8 @@ class Repository {
 }
 
 
-fun <T> times(max: Int, body: () -> T): Result<T> =
-        (0..max).asSequence().map {
+fun <T> tryTimes(@IntRange(from = 1) max: Int, body: () -> T): Result<T> =
+        (1..max).asSequence().map {
             try { Result.Success(body()) }
             catch (e: Throwable) { Result.error<T>(e) }
-        }.find { it is Result.Success } ?: Result.error(NoSuchElementException())
+        }.find { it is Result.Success } ?: Result.ignore()
